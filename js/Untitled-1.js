835b10b127fa26198fd9a31fb5024a99147015ae
Muhroads <samslivinski@gmail.com>
2022-05-26 15:37:25 -0400

    boop

--- js/Untitled-1.js
+++ js/Untitled-1.js
@@ -14,15 +14,18 @@
     // ---------------------------------------------------------------------------
     // Constants
     // ---------------------------------------------------------------------------
-      var board1 = Chessboard('board1', 'start')
-      var board2 = Chessboard('board2', {
-          draggable: true,
-          dropOffBoard: 'trash',
-          sparePieces: true
-      })
-  
-      $('#startBtn').on('click', board2.start)
-      $('#clearBtn').on('click', board2.clear)
+    var config = {
+        position: 'start'
+    }
+    var board1 = Chessboard('board1', 'config')
+    var board2 = Chessboard('board2', {
+        draggable: true,
+        dropOffBoard: 'trash',
+        sparePieces: true
+    })
+
+    $('#startBtn').on('click', board2.start)
+    $('#clearBtn').on('click', board2.clear)
     var COLUMNS = 'abcdefgh'.split('')
     var DEFAULT_DRAG_THROTTLE_RATE = 20
     var ELLIPSIS = '…'
